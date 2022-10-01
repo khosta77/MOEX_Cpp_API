e18beeefcf04701ba4d1ade9e130c80efcc1c88a
khosta77 <puwerfulpants@gmail.com>
2022-10-01 22:25:40 +0300

    Перенесены функции, которые было бы не уместно делать методами класса

--- utilities.cpp
+++ utilities.cpp
@@ -1,3 +1,47 @@
-//
-// Created by stepan on 10/1/22.
-//
+#include "utilities.h"
+
+std::string cut(const std::string &str, const char *find) {
+    std::string::size_type pos = str.find(find);
+    if (pos != std::string::npos)
+        return str.substr(pos);
+    return std::string();
+}
+
+std::string parser_in_data(const std::string &str, const std::string &find) {
+    std::string pr;
+    for (size_t i = (str.find(" " + find) + find.size() + 3); str[i] != '\"'; ++i) {
+        pr += str[i];
+    }
+    return pr;
+}
+
+std::vector<std::string> position::get(std::string &str, const bool &fwsic) {
+    std::vector<std::string> pos;
+    for (size_t i = 0; i < std::count(str.begin(), str.end(), '>'); ++i) {
+        pos.push_back(str.substr(0, (str.find('>') + 1)));
+        str = cut(cut(str, "\n"), "<");
+    }
+    filter_whitespace(pos);
+    if(fwsic)
+        filter_whitespace_in_cell(pos);
+    return pos;
+}
+
+void position::filter_whitespace(std::vector<std::string> &pos) {
+    int k = 0;
+    for (auto &it: pos) { // это можно красиво сделать
+        if (it.find("<") == 0)
+            break;
+        ++k;
+    }
+    pos.erase(pos.begin(), (pos.begin() + k));
+}
+
+void position::filter_whitespace_in_cell(std::vector<std::string> &pos) {
+    std::vector<std::string> pos_filt;
+    for (auto &it : pos)
+        if (atof(parser_in_data(it, "OPEN").c_str()) != 0)
+            pos_filt.push_back(it);
+    std::swap(pos, pos_filt);
+    pos_filt.clear();
+}
--- utilities.h
+++ utilities.h
@@ -1,8 +1,24 @@
-//
-// Created by stepan on 10/1/22.
-//
-
 #ifndef MOEXPARSER_UTILITIES_H
 #define MOEXPARSER_UTILITIES_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+/** \brief Обрезает \param str до определенного \param find символа/объекта
+ * \return возращается строка после параметра find
+ * */
+std::string cut(const std::string &str, const char *find);
+
+/** \brief Находит в строке \param str объект \param find и возращает значение после него в ""
+ * */
+std::string parser_in_data(const std::string &str, const std::string &find);
+
+struct position {
+    static std::vector<std::string> get(std::string &str, const bool &fwsic = true);
+private:
+    static void filter_whitespace(std::vector<std::string> &pos);
+    static void filter_whitespace_in_cell(std::vector<std::string> &pos);
+};
 
 #endif //MOEXPARSER_UTILITIES_H
