50a1f8b670fd322f22ef04444cded894d5f44f9c
khosta77 <puwerfulpants@gmail.com>
2022-11-01 21:54:33 +0300

    Проект компилируется, ссылка корректная формируется, но детально еще ничего не проверялось. Код не причесан

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
-//#include "SL_MOEX.h"
+#include "HMOEX.h"
 
 using namespace std;
 
 
-//// http://iss.moex.com/iss/history/engines/stock/markets/shares/boa
-////rds/tqbr/securities.xml?date=2013-12-20
-//
-///** // https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/RUAL.xml?from=2022-09-29&till=2022-09-29
-// * Вот эта ссылка позволит получить к открытии закритию рус акции в определенную дату
-// * \param history
-// *
-// * \param
-// * */
 
 #include "Date.h"
 
 int main() {
-    Date d(2022, 10, 10);
-    cout << d.date() << endl;
+    moex_history mh;
+    [[maybe_unused]] auto df = mh.parser("MTSS");
+//    Date d(2022, 10, 10);
+//    cout << d.date() << endl;
 //    MOEX_parser moex;
 //    auto df = moex.parser("GAZP");
 //    cout << df["DATE"] << " " << df["OPEN"] << endl;
