2f58bc4276a1bf8bac922cbd5dfe1a1c2e80b063
khosta77 <puwerfulpants@gmail.com>
2022-11-09 22:52:54 +0300

    Поверхностный запуск данных внутри дня

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "HMOEX.h"
+#include "DMOEX.h"
 
 using namespace std;
 
 #include "Date.h"
 
 int main() {
-    moex_history mh;
-    Date d1("2022-10-31"), d2("2022-11-3");
-    [[maybe_unused]] auto df = mh.parser("MTSS", d1, d2);
+    moex_day md;
+    // Date d1("2022-10-31"), d2("2022-11-3");
+    [[maybe_unused]] auto df = md.parser("MTSS");
     return 0;
 }
