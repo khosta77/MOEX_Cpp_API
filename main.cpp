4379e1ac7ffcdd6d2ae3896631e57407f3f78f70
khosta77 <puwerfulpants@gmail.com>
2022-11-10 20:32:44 +0300

    Просто проверки.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "DMOEX.h"
+#include "Time.h"
 
 using namespace std;
 
@@ -9,5 +10,7 @@ int main() {
     moex_day md;
     // Date d1("2022-10-31"), d2("2022-11-3");
     [[maybe_unused]] auto df = md.parser("MTSS");
+    Time t("22:12:3");
+    cout << t.time() << endl;
     return 0;
 }
