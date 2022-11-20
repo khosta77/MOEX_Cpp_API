3ad2bef5573d2220800993d3811e92f7ca5f3e39
khosta77 <puwerfulpants@gmail.com>
2022-11-20 20:59:52 +0300

    Написан тест первой версии

--- RtMOEX.h
+++ RtMOEX.h
@@ -14,6 +14,48 @@
 #include "Time.h"
 #include "utilities.h"
 
+#if TEST_METHODS || TEST_CLASS
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+#define LOG "main.log"
+#define TEST_SIZE 100
+#endif
+
+#if TEST_METHODS
+std::vector<int> t_day;       // 1
+std::vector<int> t_target;    // 2
+std::vector<int> t_cut;       // 3
+std::vector<int> t_substr;    // 4
+// std::vector<int> t_split;     // 5
+std::vector<int> t_get;       // 6
+std::vector<int> t_pars;      // 7
+std::vector<int> t_cndls;     // 8
+std::vector<int> t_cndl;      // 9
+
+auto start = std::chrono::steady_clock::now();
+auto end = std::chrono::steady_clock::now();
+auto t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+void saveTestResult(const std::vector<int> &df, const std::string &name) {
+    double sum_df = 0;
+    for (auto it : df)
+        sum_df += it;
+    sum_df /= (df.size() - 1);
+    std::cout << ("Среднее время просчитывания " + name + ": ") << sum_df << " ms" << std::endl;
+    std::fstream fout(LOG, std::ios::out | std::ios::app);
+    if (fout.is_open()) {
+        fout << (">" + name) << std::endl;
+        fout << "-->" << sum_df << " ms" << std::endl;
+        for(size_t i = 0; i < df.size(); ++i)
+            fout << "--->"<< i << " - " << df[i] << " ms" << std::endl;
+    }
+    fout.close();
+}
+#endif
 
 class RtMOEX {
 protected:
@@ -55,14 +97,29 @@ protected:
     }
 
     std::string get_request_to_MOEX_in_the_format_xml(const std::string &SECID, Date first = Date(), Date last = Date()) {
+#if TEST_METHODS
+        start = std::chrono::steady_clock::now();
+#endif
         if (first == Date())
             --first;
-
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_day.push_back(t);
+#endif
         namespace http = boost::beast::http;
 
         const std::string host = HOST;
+#if TEST_METHODS
+        start = std::chrono::steady_clock::now();
+#endif
         const std::string target = get_target_form(SECID, first, last);
-
+        saveas(target, "target.txt");
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_target.push_back(t);
+#endif
         // I/O контекст, необходимый для всех I/O операций
         boost::asio::io_context ioc;
 
@@ -97,8 +154,23 @@ protected:
     }
 
     std::vector<std::string> split_the_request_into_rows(const std::string &df) {
+#if TEST_METHODS
+        start = std::chrono::steady_clock::now();
+#endif
         std::string s = cut(df, "<row ");
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_cut.push_back(t);
+        start = std::chrono::steady_clock::now();
+#endif
         s = s.substr(0, s.find("</rows>"));
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_substr.push_back(t);
+
+#endif
         return position::get(s);
     }
 
@@ -107,23 +179,52 @@ protected:
 
 public:
     RtMOEX() = default;
-    ~RtMOEX() = default;
+    ~RtMOEX() {
+#if TEST_METHODS
+        saveTestResult(t_day, "просчет дня");
+        saveTestResult(t_target,"просчет сборки ссылки");
+        saveTestResult(t_cut,"вырезание начала строки");
+        saveTestResult(t_substr,"вырезание конца строки");
+        //saveTestResult(t_split,"время разбиения строки");
+        saveTestResult(t_get,"время запроса");
+        saveTestResult(t_pars,"время парсинга");
+        saveTestResult(t_cndls,"время получения массива свеч");
+        saveTestResult(t_cndl,"время получения одной свечи");
+#endif
+    }
 
     boost::variant<std::vector<Candle>, Candle> parser(const std::string &secid, Date first = Date(), Date last = Date()) {
         /*
          * Делаем get-запрос к серверу, чтобы получить информаци об акции. Информаци получаем ввиде единой строки,
          * в ней будет как лишняя, так и нужная информация.
          * */
+#if TEST_METHODS
+        start = std::chrono::steady_clock::now();
+#endif
         std::string df = get_request_to_MOEX_in_the_format_xml(secid, first, last);
-
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_get.push_back(t); //
+#endif
         /*
          * Очищаем полученный запрос от лишней информации и разбиваем его на строки
          * */
-        std::vector<std::string> parsed_df = split_the_request_into_rows(df); saveas(parsed_df[0], "daydt.txt");
-
+#if TEST_METHODS
+        start = std::chrono::steady_clock::now();
+#endif
+        std::vector<std::string> parsed_df = split_the_request_into_rows(df);
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_pars.push_back(t); 
+#endif
         /*
          * Получаем массив свечей, распарсив строку
          * */
+#if TEST_METHODS
+        start = std::chrono::steady_clock::now();
+#endif
         if (!histoty_status().empty()) {
             std::vector<Candle> cndls;
             for (auto it : parsed_df) {
@@ -131,18 +232,30 @@ public:
                                               float(std::atof(parser_in_data(it, "CLOSE").c_str())),
                                               float(std::atof(parser_in_data(it, "LOW").c_str())),
                                               float(std::atof(parser_in_data(it, "HIGH").c_str()))), 
-                                       Date(parser_in_data(it, "TRADEDATE"))));
+                                       Date(parser_in_data(it, "TRADEDATE")))); // this
             }
-
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_cndls.push_back(t); //       start = std::chrono::steady_cloc
+#endif
             return cndls;
         }
-        
+#if TEST_METHODS
+        start = std::chrono::steady_clock::now();
+
+#endif  
         Candle cndl(Candle(float(std::atof(parser_in_data(parsed_df[0], "OPEN").c_str())),
                            float(std::atof(parser_in_data(parsed_df[0], "CLOSE").c_str())),
                            float(std::atof(parser_in_data(parsed_df[0], "LOW").c_str())),
                            float(std::atof(parser_in_data(parsed_df[0], "HIGH").c_str()))),
-                    Time(parser_in_data(parsed_df[0], "TIME")));
+                    Time(parser_in_data(parsed_df[0], "TIME"))); // this
+#if TEST_METHODS
+        end = std::chrono::steady_clock::now();
+        t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        t_cndl.push_back(t); // start = std::chrono::steady_clock::now();
 
+#endif
         return cndl;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-#include "GuideMOEX.h"
+#include "HMOEX.h"
 
 using namespace std;
 
 
 int main() {
-    guideMOEX md;
-    [[maybe_unused]] auto df = md.engines();
+#if TEST_METHODS
+    moex_history mh;
+    for (size_t i = 0; i < TEST_SIZE; ++i) 
+       [[maybe_unused]] auto df = mh.parser("GAZP");
+#endif
+/*    [[maybe_unused]] auto df = md.engines();
     cout << df.size() << endl;
     for(auto it : df) {
         cout << it.first << " " << it.second << endl;
@@ -16,6 +20,7 @@ int main() {
     [[maybe_unused]] auto df2 = md.markets(df[0].first);
     for(auto it : df2) {
         cout << it.first << " | " << it.second << endl;
-    }
+*/
+
     return 0;
 }
