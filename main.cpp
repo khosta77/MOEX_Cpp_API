8800971ce04d0ec12d4a1b78a2e1d05f47aa2924
khosta77 <puwerfulpants@gmail.com>
2022-11-20 23:08:53 +0300

    Реализован простейший тест

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include "Date.h"
+#include "Candle.h"
 #include "HMOEX.h"
-
 using namespace std;
 
 
 int main() {
 #if TEST_METHODS
     moex_history mh;
-    for (size_t i = 0; i < TEST_SIZE; ++i) 
-       [[maybe_unused]] auto df = mh.parser("GAZP");
+    for (size_t i = 0; i < TEST_SIZE; ++i) { // std::vector<Candle>, 
+       std::vector<Candle> df = mh.parser("GAZP", Date("2022-11-14"), Date("2022-11-18"));
+       for (auto it : df) 
+           cout << it.df_candle["OPEN"] << " " << it.date.date() << endl;
+    }
 #endif
 /*    [[maybe_unused]] auto df = md.engines();
     cout << df.size() << endl;
